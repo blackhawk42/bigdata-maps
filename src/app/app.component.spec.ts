a565810f27b9e956a45d126debd20fcd9d7ea5f0
Daniel Chávez <danielsaul96@gmail.com>
2020-09-09 12:58:11 -0500

    test fails

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -27,7 +27,7 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('bigdata-maps');
   }));
 
-  xit(' -- should render title in a h1 tag', async(() => {
+  it(' -- should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
